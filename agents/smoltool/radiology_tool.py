93781b123e3cc8673b404f0c11eb02e7aee1783a
laivietanh <lai.anh@ttekglobal.com>
2025-06-15 17:18:16 +0700

    code for radiology

--- agents/smoltool/radiology_tool.py
+++ agents/smoltool/radiology_tool.py
@@ -1,4 +1,4 @@
-
+import logging
 from dotenv import load_dotenv
 from smolagents import tool
 
@@ -6,34 +6,19 @@ load_dotenv()
 
 
 
+
 @tool
-def scrape_page_with_jina_ai(url: str) -> str:
-    """Scrapes content from a webpage using Jina AI's web scraping service.
+def diagnose(query:str) -> str:
+    """Diagnose base on medical query.
 
     Args:
-        url: The URL of the webpage to scrape. Must be a valid web address to extract content from.
+        query: medical query ask about symptoms and medical history.
 
     Returns:
-        str: The scraped content in markdown format.
+        str: Insights or analysis of medical in text format.
     """
-    print(f"Scraping Jina AI..: {url}")
-    # response = requests.get("https://r.jina.ai/" + url, headers=headers)
-    response = requests.get("https://r.jina.ai/" + url)
 
-    markdown_content = response.text
+    return f"you're very healthy ."
 
-    return markdown_content
-@tool
-def radiology_tool(image_path: str) -> str:
-    """Analyzes a radiology image and provides insights.
 
-    Args:
-        image_path: The path to the radiology image file.
 
-    Returns:
-        str: Insights or analysis of the radiology image.
-    """
-    print(f"Analyzing radiology image at: {image_path}")
-    # Here you would implement the logic to analyze the image
-    # For now, we return a placeholder response
-    return f"you have uploaded a radiology image at {image_path}. the result is : you're very healthy ."
--- application.py
+++ application.py
@@ -5,10 +5,12 @@ from smolagents import (
     CodeAgent,
     ToolCallingAgent,
     LiteLLMModel,
-
+    HfApiModel,
     GradioUI
 
 )
+from agents.smoltool.radiology_tool import diagnose
+from agents.smoltool.document_tool import document_tool
 from smolagents import ManagedAgent
 import os
 
@@ -18,42 +20,48 @@ class MedicalAssistantFramework:
         logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S %z')
         self.log("Medical Assistant Framework initialized")
         load_dotenv()
-        # init the model
-        self.model = LiteLLMModel(model_id="gemini/gemma-3-27b-it",
+        # Get API key from environment variables
+        # self.api_key = os.getenv("HF_TOKEN")
+        # # init the model
+
+        # self.model = HfApiModel(model_id="meta-llama/Llama-3.3-70B-Instruct")
+        self.model = LiteLLMModel(model_id="gemini/gemini-2.0-flash-exp",
                      api_key=os.getenv("GEMINI_API_KEY"))
         self.radiology_agent = ToolCallingAgent(
             model=self.model,
-            tools=["radiology_tool"]
+            tools=[diagnose],
+            max_steps=3
         )
         self.radiology_managed_agent = ManagedAgent(
-            name="Radiology Managed Agent",
+            name="super_radiology",
             agent=self.radiology_agent,
-            description="I am a radiology expert. I can assist with interpreting medical images and providing insights on radiological findings.",
+            description="got medical question and check and analyze radiology medical symbol.",
         )
-        self.document_agent = ToolCallingAgent(
-            model=self.model,
-            tools=["document_tool"]
-        )
-        self.document_managed_agent = ManagedAgent(
-            name="Document Managed Agent",
-            agent=self.document_agent,
-            description="query and retrieve medical documents, articles, and research papers.",
-        )
+        # self.document_agent = CodeAgent(
+        #     model=self.model,
+        #     tools=[document_tool]
+        # )
+        # self.document_managed_agent = ManagedAgent(
+        #     name="Medical Document Managed Agent",
+        #     agent=self.document_agent,
+        #     description="query and retrieve medical documents, articles, and research papers.",
+        # )
         self.docter_agent = CodeAgent(
             tools = [],
             model=self.model,
-            managed_agent = [self.radiology_managed_agent, self.document_managed_agent],
-            name="Doctor Agent",
-             additional_authorized_imports=["re"],
+            managed_agents = [self.radiology_managed_agent],
+            additional_authorized_imports=["re"],
 
-        # system_prompt="""You are a blog post creation manager. Coordinate between research, writing, and editing teams.
-        # Follow these steps:
-        # 1. Use research_agent to gather information
-        # 2. Pass research to research_checker_agent to check for relevance
-        # 3. Pass research to writer_agent to create the initial draft
-        # 4. Send draft to editor for final polish
-        # 4. Save the final markdown file
-        # """
+            # system_prompt="""{{authorized_imports}}
+            #   You are a medical doctor with expertise in diagnosing and treating various conditions.
+            #   If the user's question is not related to medical topics, answer it directly as a knowledgeable assistant.
+            #     You will be provided with patient symptoms and medical history, and your task is to provide a diagnosis and treatment plan.
+            #     you have access to the following agents:{{managed_agents_descriptions}}
+            #     If the user's question is medical in nature (e.g., about symptoms, diagnosis, or treatment)
+            #     1. First ask radiology_agent : for get medical advice.
+            #     3. Finally, provide a comprehensive response based on the information gathered.
+
+            # """
         )
     def doctor_agent_chat(self,message: str,history:str)-> str:
         """
@@ -65,17 +73,9 @@ class MedicalAssistantFramework:
             conversation.append(history)
         conversation.append({"role": "user", "content": message})
         self.log("Conversation history: {}".format(conversation))
-        return self.docter_agent.run(f"""
-            You are a medical doctor with expertise in diagnosing and treating various conditions.
-            You will be provided with patient symptoms and medical history, and your task is to provide a diagnosis and treatment plan.
+        result =  self.docter_agent.run(f"""{message}""")
+        return result
 
-            If the user's question is medical in nature (e.g., about symptoms, diagnosis, or treatment)
-            1. First ask Radiology Agent: For interpreting medical images and providing insights on radiological findings.
-            2. Then querying and retrieving medical documents, articles.
-            3. Finally, provide a comprehensive response based on the information gathered.
-            If the user's question is not related to medical topics, answer it directly as a knowledgeable assistant.
-            {message}
-        """)
 
     def log(self, message: str):
         logging.info(message)
@@ -83,7 +83,9 @@ class MedicalAssistantFramework:
     def chatInitialization(self):
         self.log("Chat initialization started")
         # gr.ChatInterface(fn=self.doctor_agent_chat, type="messages").launch()
-        GradioUI(self.docter_agent).launch()
+        # GradioUI(self.docter_agent).launch()
+        self.docter_agent.run("I was in the rain for 2 hours yesterday, do I need to take medicine?")
+         # Initialize the Gradio UI
         self.log("Chat initialization completed")
 
     def run(self) -> None:
